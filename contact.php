c0b9683e927aae7a1f0f57c05430b80f87360127
Abhishek Arya <aryaabhishek0120@gmail.com>
2021-01-09 18:37:28 +0530

    initial commit

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$mobile = $_POST['mobile'];
+$message = $_POST['message'];
+
+$email_from = 'Kavita Memorial Nursing Home';
+$email_subject = 'New message from KMNH contact';
+$email_body = "Name: $name.\n".
+              "Email: $email.\n".
+              "Mobile: $mobile.\n".
+              "Message: $message.\n";
+$to ="abhi.arya509@gmail.com";
+$headers ="From: $email_from \r\n";
+$headers .="Reply-To: $email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
+?>
